ad8004d84ffbd1e0b473f31b61497783346db1bb
yasbu <106036163+yasbu@users.noreply.github.com>
2022-08-24 12:03:23 +0300

    Update Odev_10.sql

--- Odev_10.sql
+++ Odev_10.sql
@@ -17,5 +17,5 @@ SELECT payment.payment_id, customer.first_name, customer.last_name FROM customer
 RIGHT JOIN payment ON customer.customer_id = payment.customer_id;
 
 --Sorgu_3
-SSELECT rental.rental_id, customer.first_name, customer.last_name FROM customer
+SELECT rental.rental_id, customer.first_name, customer.last_name FROM customer
 FULL JOIN rental ON customer.customer_id = rental.customer_id;
